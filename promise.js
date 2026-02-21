a076a96f80ebedb8be61bc0e82c3665344f919df
stoXmod <nisharasenadheera2@gmail.com>
2026-02-21 18:43:46 +0530

    feat: Add promise implementation with success and failure handling

---
+++ promise.js
@@ -0,0 +1,15 @@
+const condition = true; // Change this to false to see the rejection case
+
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
